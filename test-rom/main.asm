48753d5ace1cb629ff16db64478d135053ff90ce
Frode Austvik <edorfaus@xepher.net>
2024-05-31 01:54:06 +0200

    Add some test code to verify the generated values

--- test-rom/main.asm
+++ test-rom/main.asm
@@ -1,22 +1,100 @@
 .include "core.asm"
 .include "graphics.asm"
+.include "testdata.asm"
 
 .include "../xoshiro/128plus.asm"
 .include "../xoshiro/128next.asm"
 
+.segment "ZEROPAGE"
+verifyValues: .res 2
+verifyStates: .res 2
+
 .segment "CODE"
 
 Main:
+	jsr InitState_Xoshiro128
+
 	jsr Xoshiro128Plus
+	jsr verifyValueXoshiro128
+	jsr verifySameStateXoshiro128
+
 	jsr Xoshiro128Next
-
-	lda #1
-	jsr WriteTile
-
-	lda #2
-	jsr WriteTile
+	jsr verifyNextStateXoshiro128
 
 	lda #3
 	jsr WriteTile
 
 :	jmp :-
+
+; Clobbers: A, Y
+InitState_Xoshiro128:
+	; This assumes that the state variables are consecutive in memory.
+	ldy #4*.sizeof(Xoshiro128State0)-1
+	:
+		lda Seed, y
+		sta Xoshiro128State0, y
+		dey
+	bpl :-
+
+	lda #.lobyte(VerifyValuesXoshiro128)
+	sta verifyValues+0
+	lda #.hibyte(VerifyValuesXoshiro128)
+	sta verifyValues+1
+
+	lda #.lobyte(VerifyStatesXoshiro128)
+	sta verifyStates+0
+	lda #.hibyte(VerifyStatesXoshiro128)
+	sta verifyStates+1
+
+	rts
+
+; Clobbers: A, Y
+verifyValueXoshiro128:
+	ldy #.sizeof(Xoshiro128Value)-1
+	@loop:
+		lda (verifyValues), y
+		cmp Xoshiro128Value, y
+		bne @failed
+	dey
+	bpl @loop
+	@failed:
+	clc
+	lda verifyValues+0
+	adc #.sizeof(Xoshiro128Value)
+	sta verifyValues+0
+	bcc :+
+		inc verifyValues+1
+	:
+	jmp showVerifyResult
+
+; Clobbers: A, Y
+verifyNextStateXoshiro128:
+	clc
+	lda verifyStates+0
+	adc #4*.sizeof(Xoshiro128State0)
+	sta verifyStates+0
+	bcc :+
+		inc verifyStates+1
+	:
+	; Fall through to verifySameStateXoshiro128
+
+; Clobbers: A, Y
+verifySameStateXoshiro128:
+	; This assumes that the state variables are consecutive in memory.
+	ldy #4*.sizeof(Xoshiro128State0)-1
+	@loop:
+		lda (verifyStates), y
+		cmp Xoshiro128State0, y
+		bne showVerifyResult
+	dey
+	bpl @loop
+	; Fall through to showVerifyResult
+
+; Clobbers: A
+showVerifyResult:
+	lda #1
+	cpy #$FF
+	beq :+
+		lda #2
+	:
+	jmp WriteTile
---
+++ test-rom/testdata.asm
@@ -0,0 +1,24 @@
+.segment "DATA"
+
+VerifyValuesXoshiro128:
+	; plus
+	.dword $C6543AFC
+	.dword $D4843189
+	.dword $D7559586
+	.dword $9C3FC0E5
+
+VerifyStatesXoshiro128:
+	.dword $964D4A0C, $D40AB895, $AD7FDECB, $3006F0F0
+	.dword $72410269, $EF382C52, $2E43BEC7, $62432F20
+	.dword $FF3A011B, $B33A90FC, $2C5A18AE, $D81B946B
+	.dword $941B058C, $605A8949, $A641E1B5, $0824BB59
+	.dword $FC65379C, $52006D70, $87487639, $F1908343
+
+; Random data used to seed the PRNG (by setting its initial state).
+; This is the same data that was used to initialize the C reference
+; implementation to generate the other test data used for verification.
+Seed:
+	.byte $0C, $4A, $4D, $96, $95, $B8, $0A, $D4
+	.byte $CB, $DE, $7F, $AD, $F0, $F0, $06, $30
+	.byte $08, $FF, $21, $21, $A5, $AA, $23, $A2
+	.byte $8B, $EE, $C2, $2E, $04, $0E, $DF, $2F
